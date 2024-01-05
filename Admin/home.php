60554fae735d5eeae9cb48de2a55d0db8f989e06
satya-901 <satyendrasharmasatya4@gmail.com>
2024-01-05 12:49:00 +0530

    adding Dashboard

--- Admin/home.php
+++ Admin/home.php
@@ -50,15 +50,15 @@
     <div class="col-xxl-4 col-md-6">
         <div class="card info-card sales-card">
             <div class="card-body">
-                <h5 class="card-title">Sales <span>| Today</span></h5>
+                <h5 class="card-title">Service</h5>
                 <div class="d-flex align-items-center">
                     <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                         <i class="bi bi-cart"></i>
                     </div>
                     <div class="ps-3">
-                        <h6>145</h6>
-                        <span class="text-success small pt-1 fw-bold">12%</span> <span
-                            class="text-muted small pt-2 ps-1">increase</span>
+                        <h6>
+                            <?php echo $conn->query("SELECT * FROM services")->num_rows; ?>
+                        </h6>
                     </div>
                 </div>
             </div>
@@ -69,15 +69,15 @@
     <div class="col-xxl-4 col-md-6">
         <div class="card info-card revenue-card">
             <div class="card-body">
-                <h5 class="card-title">Revenue <span>| This Month</span></h5>
+                <h5 class="card-title">Total Images</h5>
                 <div class="d-flex align-items-center">
                     <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                        <i class="bi bi-currency-dollar"></i>
+                        <i class="bi bi-card-image"></i>
                     </div>
                     <div class="ps-3">
-                        <h6>$3,264</h6>
-                        <span class="text-success small pt-1 fw-bold">8%</span> <span
-                            class="text-muted small pt-2 ps-1">increase</span>
+                        <h6>
+                            <?php echo $conn->query("SELECT * FROM gallery")->num_rows; ?>
+                        </h6>
                     </div>
                 </div>
             </div>
@@ -89,15 +89,15 @@
 
         <div class="card info-card customers-card">
             <div class="card-body">
-                <h5 class="card-title">Customers <span>| This Year</span></h5>
+                <h5 class="card-title">Total number of queries</h5>
                 <div class="d-flex align-items-center">
                     <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                         <i class="bi bi-people"></i>
                     </div>
                     <div class="ps-3">
-                        <h6>1244</h6>
-                        <span class="text-danger small pt-1 fw-bold">12%</span> <span
-                            class="text-muted small pt-2 ps-1">decrease</span>
+                        <h6>
+                            <?php echo $conn->query("SELECT * FROM contact")->num_rows; ?>
+                        </h6>
                     </div>
                 </div>
             </div>
